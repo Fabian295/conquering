d9158fb59a6c3ac636307d4df3c8965502af6363
Fabian295 <fabiankanhai70@gmail.com>
2022-11-03 01:37:51 +0100

    Create finanancesActions.js

--- src/actions/financesActions.js
+++ src/actions/financesActions.js
@@ -3,3 +3,30 @@ export const addExpense = () => {
       type: 'EXPENSES/ADD'
     }
 }
+
+export const addBY = ({incBy = 25} = {}) => {
+  return {
+    type: 'EXPENSES/INCBY',
+    incBy
+  }
+}
+
+export const minExpense = () => {
+  return {
+    type: 'EXPENSES/MIN'
+  }
+}
+
+export const minBy = ({decBy = 10} = {}) => {
+  return {
+    type: 'EXPENSES/DECBY',
+    decBy
+  }
+}
+
+export const reset = () => {
+  return {
+  type: 'EXPENSES/RESET',
+  set: 100
+  }
+}
